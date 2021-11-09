091a68c52d53a51da03ad062760732a2049f82db
Jake Gonzalez <jake.g29@outlook.com>
2021-11-08 19:09:50 -0600

    made changes

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 console.log('JavaScript code has loaded!')
 
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Jake Gonzalez" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0      // Gingerbread
@@ -14,10 +14,13 @@ document.getElementById('credit').textContent = `Created by ${yourName}`
 
 // Event listener for clicks on the "+" button for Gingerbread cookies
 document.getElementById('add-gb').addEventListener('click', function() {
-    // HINT: You can delete this console.log after you no longer need it!
-    console.log('Gingerbread + button was clicked!')
+    let gb = gb + 1
+    console.log('Gingerbread + button was clicked!')})
+
+
+
+
 
     // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
-})
 
 // TODO: Hook up event listeners for the rest of the buttons
